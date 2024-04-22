9a47188b754cf4f04892cf79994276d8bc5376c6
aspartaue <spartakvasilev1999@gmail.com>
2024-04-22 18:26:06 +0900

    feat(route): add routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\JobController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
@@ -29,10 +30,23 @@ Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('/jobs', [JobController::class, 'index'])->name('jobs.index');
+    Route::get('/jobs/create', [JobController::class, 'create'])->name('jobs.create');
+    Route::post('/jobs/create', [JobController::class, 'store'])->name('jobs.store');
+    Route::get('/jobs/manage', [JobController::class, 'manage'])->name('jobs.manage');
+    Route::get('/jobs/edit/{id}', [JobController::class, 'edit'])->name('jobs.edit');
+    Route::post('/jobs/edit/{id}', [JobController::class, 'update'])->name('jobs.update');
+    Route::get('/jobs/{id}', [JobController::class, 'show'])->name('jobs.show');
+    Route::post('/jobs/manage/{id}', [JobController::class, 'destroy'])->name('jobs.destroy');
+});
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::post('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/profile/detail', [ProfileController::class, 'show'])->name('profile.show');
+    Route::post('/profile/detail', [ProfileController::class, 'updateProfileDetails'])->name('profile.updateDetail');
 });
 
 require __DIR__.'/auth.php';
